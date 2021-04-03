a526cab86b39c6fd047d548db3a62dda32336980
Abdurrohim_ethan <abdurrohim48@gmail.com>
2021-04-03 13:00:53 +0700

    adding functionality to store the list on the local storage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem);
+  }
+  return [];
+};
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
   const [alert, setAlert] = useState({
@@ -48,6 +55,9 @@ function App() {
     const specificItem = list.find(item => item.id === id);
     setName(specificItem.title);
   };
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
