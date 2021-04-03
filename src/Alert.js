f424cb0ef9999fd4c8b0cceb380ff6dbbd9eb24a
Abdurrohim_ethan <abdurrohim48@gmail.com>
2021-04-03 13:17:41 +0700

    adding assets images

--- src/Alert.js
+++ src/Alert.js
@@ -4,7 +4,7 @@ const Alert = ({ type, msg, removeAlert }) => {
   useEffect(() => {
     setTimeout(() => {
       removeAlert();
-    }, 3000);
+    }, 10000);
   }, []);
   return <p className={`alert alert-${type}`}>{msg}</p>;
 };
