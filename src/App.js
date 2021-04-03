035e0793b65dc0d17d263c4ea81cfaeccc87a47e
Abdurrohim_ethan <abdurrohim48@gmail.com>
2021-04-03 08:36:03 +0700

    adding functionality on app.js

--- src/App.js
+++ src/App.js
@@ -12,10 +12,31 @@ function App() {
     msg: "",
     type: "",
   });
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log("hello");
+  };
   return (
     <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery Bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
       <div className="grocery-container">
         <List />
+        <button className="clear-btn">clear items</button>
       </div>
     </section>
   );
